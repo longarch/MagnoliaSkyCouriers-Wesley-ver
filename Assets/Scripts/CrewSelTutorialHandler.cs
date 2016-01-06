f247c8eefdfa2e3ca0a050af3ebd3d74d5cef9eb
Lithality <liquidous_zack@hotmail.com>
2016-01-06 22:28:18 +0900

    Added dialog samples
    Added dialog manager
    Added Tutorial Sel Handler

---
+++ Assets/Scripts/CrewSelTutorialHandler.cs
@@ -0,0 +1,201 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CrewSelTutorialHandler : MonoBehaviour {
+
+	public enum Tutorial1Phase
+	{
+		PreStart,
+		Dialog,
+		Action,
+		Ending
+	}
+
+	[SerializeField]
+	Text txtInstruction1;
+
+	[SerializeField]
+	Button tutCompleteBtn,txtNextBtn;
+
+	[SerializeField]
+	Image DialogPnl;
+
+	string emptySpeech = "";
+
+	[SerializeField]
+	List<string> Dialogs,Dialogs_2,Dialogs_3;
+	int current = 0;
+
+	public Tutorial1Phase TPhase = new Tutorial1Phase ();
+
+	private static CrewSelTutorialHandler _instance = null;
+
+	private bool _isSelectedCat, _isClickedProject, _isCrewFull;
+
+	public static CrewSelTutorialHandler Instance
+	{
+		get { return _instance; }
+	}
+
+	public bool isSelectedCat()
+	{
+		return _isSelectedCat;
+	}
+
+	public void setCatSelected()
+	{
+		_isSelectedCat = true;
+	}
+
+	public bool isSelectedProject()
+	{
+		return _isClickedProject;
+	}
+	
+	public void setProjectSelected()
+	{
+		_isClickedProject = true;
+	}
+
+	public bool isCrewFull()
+	{
+		return _isCrewFull;
+	}
+	
+	public void setCrewFull()
+	{
+		_isCrewFull = true;
+	}
+	
+	void Awake()
+	{
+		if (_instance != null && _instance != this)
+		{
+			Destroy(gameObject);
+		}
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		TPhase = Tutorial1Phase.PreStart;
+		HideCompleteBtn ();
+		_isSelectedCat = false;
+		_isClickedProject = false;
+		_isCrewFull = false;
+		if (Dialogs != null) {
+			StartCoroutine (TypeText (Dialogs [current]));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetButtonDown ("Jump") && current < Dialogs.Count - 1) {
+			
+			
+			current += 1;
+			StartCoroutine(TypeText (Dialogs[current]));
+
+
+		}
+		txtInstruction1.text = emptySpeech;
+
+	}
+
+	IEnumerator TypeText (string speech) {
+		emptySpeech = "";
+		foreach (char letter in speech) {
+			emptySpeech += letter;
+			yield return 0;
+			yield return new WaitForEndOfFrame();
+		}      
+	}
+
+	public void triggerNextDialog()
+	{
+		txtInstruction1.text = "";
+		if (current < Dialogs.Count - 1) {
+			current += 1;
+			StopAllCoroutines();
+			StartCoroutine (TypeText (Dialogs [current]));
+		}
+
+		//Last line of dialog!
+		if (current == Dialogs.Count - 1)
+		{
+			
+			tutCompleteBtn.gameObject.SetActive(true);
+		}
+
+	}
+
+	public void HideCompleteBtn()
+	{
+		tutCompleteBtn.gameObject.SetActive (false);
+	}
+
+	public void HideDialogBox()
+	{
+		TPhase = Tutorial1Phase.Action;
+		DialogPnl.gameObject.SetActive (false);
+	}
+
+	public void setUpDialog(int i)
+	{
+		TPhase = Tutorial1Phase.Dialog;
+		DialogPnl.gameObject.SetActive (true);
+		tutCompleteBtn.gameObject.SetActive (false);
+		current = 0;
+		Dialogs.Clear ();
+		switch (i) {
+		case 1:
+			for (int j = 0; j < Dialogs_2.Count; j ++) {
+				Dialogs.Add (Dialogs_2[j]);
+			}
+			break;
+		case 2:
+			for (int j= 0; j < Dialogs_3.Count; j ++) {
+				Dialogs.Add (Dialogs_3[j]);
+			}
+			break;
+		}
+
+		StopAllCoroutines();
+		StartCoroutine (TypeText (Dialogs [current]));
+
+
+		//txtNextBtn.onClick.AddListener (() => triggerNextDialog_2());
+	}
+
+	public void setUpDialog(List<string> newDialog)
+	{
+		TPhase = Tutorial1Phase.Dialog;
+		DialogPnl.gameObject.SetActive (true);
+		tutCompleteBtn.gameObject.SetActive (false);
+		current = 0;
+		Dialogs.Clear ();
+
+		for (int j = 0; j < newDialog.Count; j ++) {
+			Dialogs.Add (newDialog[j]);
+		}
+
+
+		
+		
+		StopAllCoroutines();
+		StartCoroutine (TypeText (Dialogs [current]));
+		
+		
+		//txtNextBtn.onClick.AddListener (() => triggerNextDialog_2());
+	}
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+
+public class DialogManager : MonoBehaviour {
+
+
+
+	private static DialogManager _instance = null;
+
+	public class Dialog
+	{
+
+
+
+		public List<string> texts;
+
+		public Dialog()
+		{
+
+		}
+
+		public Dialog(TextAsset assets)
+		{
+			texts = new List<string>();
+
+			if (assets != null)
+			{
+				loadXMLFromAsset(assets);
+			}
+
+		}
+
+		// Following method load xml file from resouces folder under Assets
+		private void loadXMLFromAsset(TextAsset asset)
+		{
+			//Loads XML
+			XmlDocument xmlDoc = new XmlDocument();
+			xmlDoc.LoadXml (asset.text);
+
+			//Reads XML
+			foreach (XmlElement node in xmlDoc.SelectNodes("DialogCollection/Dialogs")) {
+				for (int i = 0; i < node.SelectSingleNode("Dialog").ChildNodes.Count; i ++)
+				{
+					//p.ProjReq.Add(node);
+					texts.Add(node.SelectSingleNode("Dialog").ChildNodes[i].InnerText);
+					
+				}
+
+			}
+			//xmlDoc.Load( Application.persistentDataPath + "/Resources/Projects.xml" );
+			//TextAsset tipsFile = Resources.Load("CatData") as TextAsset;
+			//xmlDoc.Load(tipsFile.text);
+			//xmlDoc.Load(tipsFile.text);
+		}
+
+		public List<string> getListofTexts()
+		{
+			return texts;
+		}
+
+	}
+
+	[SerializeField]
+	TextAsset asset;
+
+	List<Dialog> _Dialogs;
+	
+
+
+	public static DialogManager Instance
+	{
+		get { return _instance; }
+	}
+
+
+	// Use this for initialization
+	void Awake () {
+		if (_instance != null && _instance != this)
+		{
+			Destroy(gameObject);
+		}
+		_instance = this;
+
+		_Dialogs =  new List<Dialog>();
+
+		Dialog D = new Dialog (asset);
+
+		_Dialogs.Add (D);
+
+
+
+	}
+
+
+
+	void Start() {
+
+
+	}
+
+	public Dialog returnDialogs(int i)
+	{
+		return _Dialogs[i];
+	}
+
+
+	
+
+}
