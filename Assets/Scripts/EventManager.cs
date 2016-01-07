fe1b5250f61a86483d3c588f837c875dc8ee9bf9
Lithality <liquidous_zack@hotmail.com>
2016-01-07 22:47:21 +0900

    Added camera shake effect and Event manager

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+public class EventManager : MonoBehaviour {
+
+	[SerializeField]
+	Camera innerCam;
+
+	[SerializeField]
+	float eventTime = 1.0f;
+	float fixedeventTime;
+	// Use this for initialization
+	void Start () {
+
+		fixedeventTime = eventTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		eventTime -= Time.deltaTime;
+		if (eventTime <= 0) {
+			innerCam.DOShakePosition(0.5f, 5.0f, 30);
+			eventTime = fixedeventTime;
+		}
+
+	}
+}
