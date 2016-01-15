f36aa407b58fc5cdeb2c50c58700da29f139d8f6
Soulravel <soulravel@hotmail.com>
2016-01-15 14:43:43 +0800

    Added Cloud Pool
    Used a different pool code

---
+++ Assets/Scripts/BaseCloud.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class BaseCloud : MonoBehaviour
+{
+	[SerializeField]
+	protected CloudManager cloud;
+	protected Vector3 position;
+	Vector2 min;
+	protected float heightChangeTimer = 1.0f, heightAscent = 0;
+	// Use this for initialization
+
+	void Destroy()
+	{
+		gameObject.SetActive(false);
+	}
+	void OnDisable()
+	{
+		CancelInvoke();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+		if (transform.position.x < min.x) {
+			Debug.Log ("Destroy");
+			Invoke ("Destroy", 1f);
+		}
+	}
+
+	public void Setup (Vector2 startingPos)
+	{
+		position = startingPos;
+		transform.position = position;
+	}
+
+	public float heightVariantChange ()
+	{
+		return UnityEngine.Random.Range (-0.5f, 0.5f);
+	}
+
+	public void bobCloud ()
+	{
+		heightChangeTimer -= Time.deltaTime;
+
+		if (heightChangeTimer <= 0.0f) {
+			heightAscent = heightVariantChange ();
+			heightChangeTimer = 4.0f; //Hard coded for now
+		}
+
+		//transform.DOMoveY (heightAscent, 5.0f, false);
+		transform.position = position;
+	}
+}
---
+++ Assets/Scripts/CloudManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class CloudManager : MonoBehaviour
+{
+	public float fireTime = .05f;
+	//public GameObject bullet;
+	//public int pooledAmount = 20;
+	List<GameObject> clouds;
+
+	void Start ()
+	{
+		InvokeRepeating ("Fire", fireTime, fireTime);
+	}
+
+	void Fire ()
+	{
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (1, 0));
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+
+		GameObject obj = CloudPool.current.GetPooledObject ();
+		if (obj == null)
+			return;
+		obj.transform.position = new Vector2 (Random.Range (min.x, max.x+15), Random.Range (min.y, max.y));
+		//obj.transform.rotation = transform.rotation;
+		obj.SetActive (true);
+
+	}
+}
+
